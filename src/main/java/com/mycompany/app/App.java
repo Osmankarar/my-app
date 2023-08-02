3053e7e51ea88f3b2815b639b9c87a21db4f5a05
Osmankarar <54031691+Osmankarar@users.noreply.github.com>
2023-08-02 19:26:49 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is Metro DevOps Class! 
-                           We are invoking Jenkins" );
+        System.out.println( "Hello World! This is Metro DevOps Class! We are invoking Jenkins" );
     }
 }
